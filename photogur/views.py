bebfecc3d4cb25556444669c938938dac4a86af3
Alexander G. Yang <spark8402@rogers.com>
2019-05-27 14:40:04 -0400

    assignment done

--- photogur/views.py
+++ photogur/views.py
@@ -8,6 +8,14 @@ def pictures_page(request):
     response = render(request, 'index.html', context)
     return HttpResponse(response)
 
+def picture_show(request, id):
+    picture = Picture.objects.get(pk=id)
+    context = {'picture': picture}
+    response = render(request, 'picture.html', context)
+    return HttpResponse(response)
+
+
+
 # def contacts_list(request):
 #     context = {'contacts': Contact.objects.all()}
 #     html = render(request, 'index.html', context)
