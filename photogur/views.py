b2ce12ad4676181894bc798a5589e4d20fef6bbc
Alexander G. Yang <spark8402@rogers.com>
2019-05-28 22:15:14 -0400

    assignment done

--- photogur/views.py
+++ photogur/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from photogur.models import Picture, Comment
+from django.views.decorators.http import require_http_methods
 
 
 def pictures_page(request):
@@ -14,9 +15,23 @@ def picture_show(request, id):
     response = render(request, 'picture.html', context)
     return HttpResponse(response)
 
+def picture_search(request):
+  query = request.GET['query']
+  search_results = Picture.objects.filter(artist=query)
+  context = {'pictures': search_results, 'query': query}
+  response = render(request, 'search.html', context)
+  return HttpResponse(response)  
 
 
-# def contacts_list(request):
-#     context = {'contacts': Contact.objects.all()}
-#     html = render(request, 'index.html', context)
-#     return HttpResponse(html)
+@require_http_methods(['POST'])
+def create_comment(request):
+  # this is where we'll receive the form submission
+    picture = request.POST['picture']
+    name = request.POST['name']
+    message = request.POST['message']
+    commented_picture = Picture.objects.get(pk=request.POST['picture'])
+    new_comment = Comment(picture = commented_picture , name = name, message = message)
+    new_comment.save()
+    return HttpResponseRedirect('/pictures/' + picture)   
+
+
